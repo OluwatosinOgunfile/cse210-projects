f65c66ea57b3aaba44fd7663a30db9bf64cf9ec7
Oluwatosin Ogunfile <o.ogunfile@rbdcenter.org>
2025-07-04 14:10:56 +0100

    Completed Exercises 1 and 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string  firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string  lasttName = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lasttName}, {firstName} {lasttName}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        float grade = float.Parse(Console.ReadLine());
+        string letterGrade;
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You earned a(n) {letterGrade} grade.");
+
+        if (letterGrade != "F")
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Great Attempt! Although you failed this time, I am sure you will pass this course on your next attempt with the experience you now have. Stay motivated.");
+        }
     }
 }
