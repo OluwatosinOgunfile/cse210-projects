467e1ca1067ef2ea7946e5a24c135cee5af4d200
Oluwatosin Ogunfile <o.ogunfile@rbdcenter.org>
2025-07-07 13:18:47 +0100

    Completed Exercises 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int attempts = 0;
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            attempts++;
+
+
+        } while (guess != magicNumber);
+
+        Console.WriteLine($"You made {attempts} attempts in total.");
+
     }
 }
