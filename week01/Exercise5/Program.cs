bd8163c01139b436c49737eab87673b1ecad8e66
Oluwatosin Ogunfile <o.ogunfile@rbdcenter.org>
2025-07-10 14:29:36 +0100

    Completed Exercises 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+
+        static int SquareNumber(int userNumber)
+        {
+            int sq = userNumber * userNumber;
+            return sq;
+        }
+
+        static void DisplayResult(string userName, int square)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {square}");
+        }
+
+        DisplayWelcome();
+        string user = PromptUserName();
+        int number = PromptUserNumber();
+        int result = SquareNumber(number);
+        DisplayResult(user, result);
+
     }
 }
