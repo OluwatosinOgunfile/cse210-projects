2f78276695d21b2bd26f47690a5543fb4bdfe296
Oluwatosin Ogunfile <o.ogunfile@rbdcenter.org>
2025-07-11 13:15:04 +0100

    Completed Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "Eye Foundation Hospital";
+        job1._jobTitle = "Medical Records Officer";
+        job1._startYear = 2011;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+
+        job2._company = "Summit Healthcare Limited";
+        job2._jobTitle = "Biomed Technician";
+        job2._startYear = 2017;
+        job2._endYear = 2019;
+
+        Resume myResume = new Resume();
+        myResume._name = "Oluwatosin Ogunfile";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
